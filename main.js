018aceb07839ac8eb83fdf41b1c93a706023705d
MahLuh <108954263+MahLuh@users.noreply.github.com>
2023-06-05 16:40:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+noseY = 0
+noseX = 0
+rightWrist = 0
+leftWrist = 0
+diference = 0
 function setup(){
-Canvas = CreateCanvas(1560, 400)
+   Canvas = createCanvas(1560, 400)
+   video =  createCapture(VIDEO)
+   video.size(150, 150)
+   poseNet = ml5.poseNet(video, modelLoaded)
+   poseNet.on("pose", gotPoses)
+
 }
 function draw(){
     background("white")
+    
+}
+function gotPoses(results){
+   if(results.lenght>0){
+      console.log(results)
+      noseY = results[0].pose.nose.x
+      noseX = results[0].pose.nose.y
+      leftWristX = results[0].pose.leftWrist.x
+      rightWristX = results[0].pose.rightWrist.x
+      diference = floor(leftWristX - rightWristX)
+      console.log("NoseX = " + noseX)
+      console.log("NoseY" + noseY)
+      console.log("rightWristX" + rightWristX)
+      console.log("leftWristX" + leftWristX)
+      console.log("diference carregada" + diference)
+   }
+}
+function modelLoaded(){
+      console.log ("PoseNet inicializado")
 }
